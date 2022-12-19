47605ae9116d28d3ae7cd3a094d2b7d54a63efd2
Izmaylova <Natalyatf@yandex.ru>
2022-12-19 17:26:14 +0500

    initial commit

---
+++ src/main/java/me/izm/RecipesWebApplication.java
@@ -0,0 +1,13 @@
+package me.izm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipesWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/izm/RecipesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package me.izm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
