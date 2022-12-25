b6b0577a73ec1f11c741081733890f227eaae14f
Izmaylova <Natalyatf@yandex.ru>
2022-12-26 02:21:14 +0500

    Домашнее задание Подключение библиотек Lombok, Apache commons, Swagger

--- src/main/java/me/izm/model/Ingredient.java
+++ src/main/java/me/izm/model/Ingredient.java
@@ -1,35 +1,14 @@
 package me.izm.model;
 
+import lombok.Data;
+
+@Data
 public class Ingredient {
 
-    private final String name;
+    private String name;
     private int count;
     private String measurement;
 
-    public Ingredient( String name, int count, String measurement) {
-        this.name = name;
-        this.count = count;
-        this.measurement = measurement;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getQuantity() {
-        return count;
-    }
-
-    public String getMeasurement() {
-        return measurement;
-    }
-
-    public void setQuantity(int quantity) {
-        this.count = quantity;
-    }
-
-    public void setMeasurement(String measurement) {
-        this.measurement = measurement;
-    }
 
 }
--- src/main/java/me/izm/model/Recipe.java
+++ src/main/java/me/izm/model/Recipe.java
@@ -1,35 +1,16 @@
 package me.izm.model;
 
-import java.util.*;
+import lombok.Data;
 
+import java.util.*;
+@Data
 public class Recipe {
 
-    private final String name;
+    private String name;
     private int cookingTime;
     private List<Ingredient> ingredients;
     private List<String> steps;
 
-    public Recipe(String name, int cookingTime, List<Ingredient> ingredients, List<String> steps) {
-        this.name = name;
-        this.cookingTime = cookingTime;
-        this.ingredients = ingredients;
-        this.steps = steps;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getCookingTime() {
-        return cookingTime;
-    }
-
-    public List<Ingredient> getIngredients() {
-        return ingredients;
-    }
-
-    public List<String> getSteps() {
-        return steps;
-    }
 
 }
